18990fe6e3d72af509f81059dc387f6e8a095e3d
Andrearomga <211132@ids.upchiapas.edu.mx>
2024-06-07 12:34:55 -0600

    Commit 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Bienvenidos Nueva prueba!');
+    res.send('Bienvenidos Prueba 5.0!');
 });
 
 app.listen(port, () => {
